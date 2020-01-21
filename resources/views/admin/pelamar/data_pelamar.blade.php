7dda16479fce52caf7e70dbe1e233d2e68ef0c71
andi <andy29febryanto@gmail.com>
2020-01-21 21:00:04 +0700

    export pdf

--- resources/views/admin/pelamar/data_pelamar.blade.php
+++ resources/views/admin/pelamar/data_pelamar.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Membuat Laporan PDF Dengan DOMPDF Laravel</title>
+	<title>PT. Indopsiko Indonesia</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
@@ -12,8 +12,8 @@
 		}
 	</style>
 	<center>
-		<h5>Membuat Laporan PDF Dengan DOMPDF Laravel</h4>
-		<h6><a target="_blank" href="https://www.malasngoding.com/membuat-laporan-…n-dompdf-laravel/">www.malasngoding.com</a></h5>
+		<h5>Data Seluruh Pelamar</h4>
+		<h6><a target="_blank" href="{{route('home')}}">PT. Indopsiko - Human Resource Development</a></h5>
 	</center>
 
 	<table class='table table-bordered'>
