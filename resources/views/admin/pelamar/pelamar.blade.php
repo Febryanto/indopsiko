4fcade3ba05ed75ae5eada502836e8a18dc23730
andi <andy29febryanto@gmail.com>
2020-01-21 20:12:15 +0700

    export exceln pdf

--- resources/views/admin/pelamar/pelamar.blade.php
+++ resources/views/admin/pelamar/pelamar.blade.php
@@ -47,57 +47,54 @@ kt-menu__item--open kt-menu__item--here
             </div>
             <div class="kt-portlet__head-toolbar">
                 <div class="kt-portlet__head-wrapper">
-        <div class="kt-portlet__head-actions">
-            <div class="dropdown dropdown-inline">
-                <button type="button" class="btn btn-default btn-icon-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="la la-download"></i> Export
-                </button>
-                <div class="dropdown-menu dropdown-menu-right">
-                    <ul class="kt-nav">
-                        <li class="kt-nav__section kt-nav__section--first">
-                            <span class="kt-nav__section-text">Export As</span>
-                        </li>
-                        <li class="kt-nav__item">
-                            <a href="{{route('getExcel.pelamar')}}" class="kt-nav__link">
-                                <i class="kt-nav__link-icon la la-file-excel-o"></i>
-                                <span class="kt-nav__link-text">Excel</span>
-                            </a>
-                        </li>
-                        <li class="kt-nav__item">
-                            <a href="{{route('getPdf.pelamar')}}" class="kt-nav__link">
-                                <i class="kt-nav__link-icon la la-file-pdf-o"></i>
-                                <span class="kt-nav__link-text">PDF</span>
-                            </a>
-                        </li>
-                    </ul>
+                    <div class="kt-portlet__head-actions">
+                        <div class="dropdown dropdown-inline">
+                            <button type="button" class="btn btn-default btn-icon-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="la la-download"></i> Export
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <ul class="kt-nav">
+                                    <li class="kt-nav__section kt-nav__section--first">
+                                        <span class="kt-nav__section-text">Export As</span>
+                                    </li>
+                                    <li class="kt-nav__item">
+                                        <a href="{{route('getExcel.pelamar')}}" class="kt-nav__link">
+                                            <i class="kt-nav__link-icon la la-file-excel-o"></i>
+                                            <span class="kt-nav__link-text">Excel</span>
+                                        </a>
+                                    </li>
+                                    <li class="kt-nav__item">
+                                        <a href="{{route('getPdf.pelamar')}}" class="kt-nav__link">
+                                            <i class="kt-nav__link-icon la la-file-pdf-o"></i>
+                                            <span class="kt-nav__link-text">PDF</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    &nbsp;
+                    </div>
                 </div>
             </div>
-    &nbsp;
-
-        </div>
-    </div>		</div>
         </div>
         <div class="kt-portlet__body">
             {{--  <!--begin: Search Form -->  --}}
             <div class="kt-form kt-form--label-right kt-margin-t-20 kt-margin-b-10">
-        <div class="row align-items-center">
-            <div class="col-xl-8 order-2 order-xl-1">
                 <div class="row align-items-center">
-                    <div class="col-md-4 kt-margin-b-20-tablet-and-mobile">
-                        <div class="kt-input-icon kt-input-icon--left">
-                            <input type="text" class="form-control" placeholder="Search..." id="generalSearch">
-                            <span class="kt-input-icon__icon kt-input-icon__icon--left">
-                                <span><i class="la la-search"></i></span>
-                            </span>
+                    <div class="col-xl-8 order-2 order-xl-1">
+                        <div class="row align-items-center">
+                            <div class="col-md-4 kt-margin-b-20-tablet-and-mobile">
+                                <div class="kt-input-icon kt-input-icon--left">
+                                    <input type="text" class="form-control" placeholder="Search..." id="generalSearch">
+                                    <span class="kt-input-icon__icon kt-input-icon__icon--left">
+                                        <span><i class="la la-search"></i></span>
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-
                 </div>
             </div>
-
-        </div>
-    </div>
             {{--  <!--end: Search Form -->  --}}
         </div>
             <div class="kt-portlet__body kt-portlet__body--fit">
@@ -149,8 +146,8 @@ kt-menu__item--open kt-menu__item--here
 
 
             </div>
-        </div>
     </div>
+</div>
 </div>
 
 {{--  <!-- end:: Content -->  --}}
